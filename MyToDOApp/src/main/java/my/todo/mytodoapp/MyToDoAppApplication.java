ed93e63f608c9d80b4e48b47eae45351867f83e0
ldk199662 <ldk199662@gmail.com>
2023-02-20 17:55:50 +0900

    setting

---
+++ MyToDOApp/src/main/java/my/todo/mytodoapp/MyToDoAppApplication.java
@@ -0,0 +1,13 @@
+package my.todo.mytodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyToDoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyToDoAppApplication.class, args);
+    }
+
+}
---
+++ MyToDOApp/src/test/java/my/todo/mytodoapp/MyToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package my.todo.mytodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyToDoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
